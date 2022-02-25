cde0d44287a939c89a871deea26dbb5a3d4e74c1
Prokop Hanzl <prokophanzl666@gmail.com>
2022-02-25 15:16:36 +0100

    Add scripts

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -9,4 +9,4 @@
 
 INDATA=$1
 
-cat $INDATA | sed 's/XXXXX//g' | while read line ; do echo "${line,}" ; done | tr '.,!?()*1234567890@+/„“":-\n' ' ' | tr -s ' ' | tr ' ' '\n'
+cat $INDATA | sed 's/XXXXX//g' | while read line ; do echo "${line,}" ; done | tr '.,!?()*1234567890@+/„“"\:\-[]\n^-' ' ' | tr -s ' ' | tr ' ' '\n'
