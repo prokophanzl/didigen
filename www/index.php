0e4f6b97b90313f5baf7bfb52a1474e0e6c9dbe4
Prokop Hanzl <prokophanzl666@gmail.com>
2022-02-26 11:30:47 +0100

    Fix footer link target

--- www/index.php
+++ www/index.php
@@ -370,7 +370,7 @@
 		<span id="copyright">&copy; 2022 Prokop Hanzl</span>
 		<br>
 		<a href="https://github.com/prokophanzl" target="_blank">GitHub</a> |
-		<a href="https://github.com/ProkopHanzl/dsw/issues/new/choose">Fehler melden</a> |
+		<a href="https://github.com/ProkopHanzl/dsw/issues/new/choose" target="_blank">Fehler melden</a> |
 		<a href="mailto:phanzl@dustah.com">Kontakt</a>
 	</section>
 
