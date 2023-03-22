1c1acda95950195430b714a1ed9a324cffb30227
Prokop Hanzl <prokophanzl666@gmail.com>
2023-03-22 13:07:53 +0100

    Update parse.js to work with new json structure

--- resources/js/parse.js
+++ resources/js/parse.js
@@ -1,4 +1,4 @@
-import sourceData from "./dataParsed.json" assert { type: "json" };
+import sourceData from "../data/parsed/allParsed.json" assert { type: "json" };
 
 // function that replaces all non-alphanumeric characters with an alphanumeric equivalent
 const nonAlpha = (str) => {
@@ -31,21 +31,21 @@ switch (urlParams.get("match")) {
 
 // add "count" property to each "de" object, containing the sum of all "count" properties of the "gsw" objects
 dataParsed.forEach((item) => {
-	item.count = item.gsw.reduce((acc, obj) => acc + obj.count, 0);
+	item.count = item.translations.reduce((acc, obj) => acc + obj.count, 0);
 });
 
 // for each element in joined, print a div in "#translation-parent" with its data in it
 dataParsed.forEach((item) => {
 	document.querySelector("#translation-parent").innerHTML += `
-		<div class="word-wrapper" style="--matches: ${item.count}; --count-first: ${item.gsw[0].count}">
+		<div class="word-wrapper" style="--matches: ${item.count}; --count-first: ${item.translations[0].count}">
 			<div class="word-header">
 				<div class="word-menu" onclick="toggle('${nonAlpha(item.de)}')" id="${nonAlpha(item.de)}-menu"></div>
 				<div class="word-german header-word" id="${nonAlpha(item.de)}-g">
 					<span class="word-german-span break-no-dis">${item.de}</span><span class="primary50 matches-german-span break-dis"> (${item.count})</span>
 				</div>
 				<div class="word-swiss-german header-word" id="${nonAlpha(item.de)}-sg">
-					<span class="word-swiss-german-span break-no-dis">${item.gsw[0].gsw}</span
-					><span class="primary50 matches-swiss-german-span break-dis">(${item.gsw[0].count}/${item.count})</span>
+					<span class="word-swiss-german-span break-no-dis">${item.translations[0].gsw}</span
+					><span class="primary50 matches-swiss-german-span break-dis">(${item.translations[0].count}/${item.count})</span>
 				</div>
 			</div>
 			<div class="word-more-info info-bar" id="${nonAlpha(item.de)}">
@@ -56,7 +56,7 @@ dataParsed.forEach((item) => {
 	`;
 
 	// for each "gsw" object, print a div in "#translation-bar-wrapper-${item.de}" with its data in it
-	item.gsw.forEach((item2) => {
+	item.translations.forEach((item2) => {
 		document.querySelector(`#translation-bar-wrapper-${nonAlpha(item.de)}`).innerHTML += `
 			<div style="--count: ${item2.count}">
 				<span class="translation-container">
