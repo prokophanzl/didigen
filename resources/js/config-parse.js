725e2602112569c7970119545d70aa2c4cf1d249
Prokop Hanzl <prokophanzl666@gmail.com>
2023-03-27 15:20:56 +0200

    Change config parsing to data attributes

--- resources/js/config-parse.js
+++ resources/js/config-parse.js
@@ -1,28 +1,29 @@
 // load the config.json file
 const config = await $.getJSON("resources/config/config.json");
 
-// set page title to the title property of config.json
-document.title = config.text.title;
+// for each X, set elements with data-config=X to the X property of config.json
+$("[data-config]").each((index, item) => {
+	const configKey = $(item).data("config");
 
-// set #config-title to the title property of config.json
-$("#config-title").text(config.text.title);
+	// if data-config-type is set to "text", set the text of the element to the X property of config.json
+	// if data-config-type is set to placeholder, set the placeholder of the element to the X property of config.json
+	// etc.
 
-// set #word-input to the placeholder property of config.json
-$("#word-input").attr("placeholder", config.text.searchPlaceholder);
-
-// set value of #submit to the translateButton property of config.json
-$("#submit").val(config.text.translateButton);
-
-// set .config-keyword to the keyword property of config.json
-$(".config-keyword").text(config.text.keyword);
-
-// set #config-word to the word property of config.json
-$("#config-word").text(config.text.word);
-
-// set match options to the match properties of config.json
-$("#begins").text(config.text.matchBegins);
-$("#match").text(config.text.matchExact);
-$("#contains").text(config.text.matchContains);
+	switch ($(item).data("config-type")) {
+		case "text":
+			$(item).text(config.text[configKey]);
+			break;
+		case "placeholder":
+			$(item).attr("placeholder", config.text[configKey]);
+			break;
+		case "value":
+			$(item).val(config.text[configKey]);
+			break;
+		default:
+			$(item).text(config.text[configKey]);
+			break;
+	}
+});
 
 // set #config-dialect-checkmarks to the dialects property of config.json
 $.each(config.dialects, (index, item) => {
